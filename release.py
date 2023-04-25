1c3238a970c5d7af07b59c29233406fc6434ff6f
Revant Nandgaonkar <revant.one@gmail.com>
2023-04-25 13:30:18 +0000

    fix: release script and image tag

--- release.py
+++ release.py
@@ -61,10 +61,7 @@ def git_commit_tag_push(args: argparse.Namespace):
     repo.git.commit("-m", commit_message)
 
     if app_version:
-        try:
-            repo.create_tag(app_version, message=f"Released {app_version}")
-        except git.exc.GitCommandError as e:
-            cli_print(str(e))
+        repo.create_tag(app_version, message=f"Released {app_version}")
 
     git_ssh_command = os.environ.get("GIT_SSH_COMMAND")
     if git_ssh_command:
@@ -142,9 +139,6 @@ def validate_bump(args: argparse.Namespace):
         invalid_bump_error += (
             f"Cannot bump app from {current_version} to {bump_version}\n"  # noqa: E501
         )
-
-    if args.tag:
-        is_bump_invalid = False
 
     if is_bump_invalid:
         cli_print(invalid_bump_error)
